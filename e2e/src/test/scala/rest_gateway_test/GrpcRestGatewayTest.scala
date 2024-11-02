792d57b02e9e14406e1b95ee51c05cf1c007415a
sfali <syed.f.ali@improving.com>
2024-11-02 16:11:37 -0400

    updated test

--- e2e/src/test/scala/rest_gateway_test/GrpcRestGatewayTest.scala
+++ e2e/src/test/scala/rest_gateway_test/GrpcRestGatewayTest.scala
@@ -1,16 +1,20 @@
 package rest_gateway_test
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder
+import com.google.rpc.Code
+import io.grpc.StatusRuntimeException
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import rest_gateway_test.api.model.common.{TestRequestB, TestResponseB}
+import rest_gateway_test.api.model.common.{TestRequestA, TestRequestB, TestResponseA, TestResponseB}
 import rest_gateway_test.api.scala_api.TestServiceA.TestServiceAGrpc
 import rest_gateway_test.api.scala_api.TestServiceB.TestServiceBGrpc
 import rest_gateway_test.server.GrpcServer
 
 import java.util.concurrent.{ExecutorService, Executors}
+import scala.util.{Failure, Success, Try}
 
-class GrpcRestGatewayTest extends AnyWordSpec with Matchers {
+class GrpcRestGatewayTest extends AnyWordSpec with Matchers with BeforeAndAfterAll {
 
   private val Threads = 4
   private lazy val grpcChannel = GrpcServer.getGrpcClient
@@ -18,20 +22,44 @@ class GrpcRestGatewayTest extends AnyWordSpec with Matchers {
   private lazy val serviceBStub = TestServiceBGrpc.blockingStub(grpcChannel)
   private lazy val restClient = new RestGatewayClient
 
-  GrpcServer.startGateWayServer(gatewayServerExecutorSvc)
-  GrpcServer.startGrpcServer(grpcServerExecutorSvc, blockUntilShutdown = false)
+  private val gatewayServer = GrpcServer.startGateWayServer(gatewayServerExecutorSvc)
+  private val grpcServer = GrpcServer.startGrpcServer(grpcServerExecutorSvc, blockUntilShutdown = false)
+
+  override protected def afterAll(): Unit = {
+    super.afterAll()
+    gatewayServer.stop()
+    grpcServer.stop()
+  }
 
   "GrpcRestGateway" should {
-    "Simple GRPC call to service B" in {
+    "make simple GRPC call to service B" in {
       val requestId = 1L
       val response = serviceBStub.process(TestRequestB(requestId))
       response shouldBe TestResponseB(success = true, requestId = requestId, result = s"RequestId: $requestId")
     }
 
-    "Simple rest call to service B" in {
+    "make simple REST call to service B" in {
       val requestId = 1L
       val actual = restClient.postProcessServiceB(requestId)
       actual shouldBe TestResponseB(success = true, requestId = requestId, result = s"RequestId: $requestId")
+    }
+
+    "make GRPC call to service A with non-existing request id" in {
+      val requestId = 1L
+      Try {
+        serviceAStub.getRequest(TestRequestA(requestId))
+      } match {
+        case Failure(ex: StatusRuntimeException) =>
+          val message = s"RequestId {$requestId} not found"
+          ex.getMessage shouldBe s"NOT_FOUND: $message"
+
+          val status = ex.getStatus
+          status.getCode.value() shouldBe Code.NOT_FOUND.getNumber
+          status.getDescription shouldBe message
+
+        case Failure(ex) => fail(ex.getMessage)
+        case Success(_)  => fail("Test failed")
+      }
     }
   }
 
