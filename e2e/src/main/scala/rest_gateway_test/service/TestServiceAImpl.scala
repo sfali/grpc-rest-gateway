ab240e8e270f0758ffea283b56238155116c81d7
sfali <syed.f.ali@improving.com>
2024-11-02 16:10:09 -0400

    error handling and updated service

--- e2e/src/main/scala/rest_gateway_test/service/TestServiceAImpl.scala
+++ e2e/src/main/scala/rest_gateway_test/service/TestServiceAImpl.scala
@@ -1,11 +1,14 @@
 package rest_gateway_test.service
 
+import com.google.rpc.{Code, Status}
+import io.grpc.protobuf.StatusProto
 import org.slf4j.LoggerFactory
 import rest_gateway_test.api.model.common.{TestRequestA, TestResponseA}
 import rest_gateway_test.api.scala_api.TestServiceA.TestServiceAGrpc
 
 import scala.collection.mutable
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 class TestServiceAImpl extends TestServiceAGrpc.TestServiceA {
 
@@ -14,22 +17,73 @@ class TestServiceAImpl extends TestServiceAGrpc.TestServiceA {
 
   override def getRequest(request: TestRequestA): Future[TestResponseA] = {
     val requestId = request.requestId
-    logger.info("Serving get request: {}", requestId)
-    cache.get(requestId) match {
-      case Some(response) => Future.successful(response)
-      case None           => Future.failed(new IllegalArgumentException(s"Invalid requestId: $requestId"))
+    Try(validateRequestId(requestId)) match {
+      case Failure(ex)              => Future.failed(ex)
+      case Success(value) if !value =>
+        // this should never happen
+        Future.failed(
+          StatusProto.toStatusRuntimeException(
+            Status
+              .newBuilder()
+              .setCode(Code.INTERNAL_VALUE)
+              .setMessage("Unknown state")
+              .build()
+          )
+        )
+      case Success(_) =>
+        logger.info("Serving get request: {}", requestId)
+        cache.get(requestId) match {
+          case Some(response) => Future.successful(response)
+          case None =>
+            Future.failed(
+              StatusProto.toStatusRuntimeException(
+                Status
+                  .newBuilder()
+                  .setCode(Code.NOT_FOUND_VALUE)
+                  .setMessage(s"RequestId {$requestId} not found")
+                  .build()
+              )
+            )
+        }
+
     }
   }
 
   override def process(request: TestRequestA): Future[TestResponseA] = {
     val requestId = request.requestId
-    logger.info("Serving process request: {}", requestId)
-    val response =
-      cache.get(requestId) match {
-        case Some(response) => response
-        case None =>
-          TestResponseA(requestId = requestId, color = getRandomColor, transactionId = requestId.toProtoUUID)
-      }
-    Future.successful(response)
+    Try(validateRequestId(requestId)) match {
+      case Failure(ex)              => Future.failed(ex)
+      case Success(value) if !value =>
+        // this should never happen
+        Future.failed(
+          StatusProto.toStatusRuntimeException(
+            Status
+              .newBuilder()
+              .setCode(Code.INTERNAL_VALUE)
+              .setMessage("Unknown state")
+              .build()
+          )
+        )
+      case Success(_) =>
+        logger.info("Serving process request: {}", requestId)
+        val response =
+          cache.get(requestId) match {
+            case Some(response) => response
+            case None =>
+              TestResponseA(requestId = requestId, color = requestId.getColor, transactionId = requestId.toProtoUUID)
+          }
+        Future.successful(response)
+    }
   }
+
+  private def validateRequestId(requestId: Long) =
+    if (requestId <= 0) {
+      throw StatusProto.toStatusRuntimeException(
+        Status
+          .newBuilder()
+          .setCode(Code.INVALID_ARGUMENT_VALUE)
+          .setMessage(s"RequestId {$requestId} is not valid")
+          .build()
+      )
+    } else true
 }
--- e2e/src/main/scala/rest_gateway_test/service/package.scala
+++ e2e/src/main/scala/rest_gateway_test/service/package.scala
@@ -3,17 +3,16 @@ package rest_gateway_test
 import rest_gateway_test.api.model.common.{Color, UUID => ProtoUUID}
 
 import java.util.UUID
-import scala.util.Random
 
 package object service {
 
   implicit class RequestIdOps(src: Long) {
     def toProtoUUID: ProtoUUID = UUID.nameUUIDFromBytes(src.toString.getBytes).toProtoUUID
+
+    def getColor: Color = Color.fromValue((src % Color.values.size).toInt)
   }
 
   implicit class UUIDOps(src: UUID) {
-    def toProtoUUID: ProtoUUID = ProtoUUID(src.getMostSignificantBits, src.getLeastSignificantBits)
+    def toProtoUUID: ProtoUUID = ProtoUUID(src.toString)
   }
-
-  def getRandomColor: Color = Color.fromValue(Random.nextInt(100) % Color.values.size)
 }
