911ed90cdf6f657cc8b9032e811e23a19a4e39c0
sfali <syed.f.ali@improving.com>
2024-11-10 07:42:36 -0500

    extract delegate functions [minor] #11

--- code-gen/src/main/scala/com/improving/grpc_rest_gateway/compiler/package.scala
+++ code-gen/src/main/scala/com/improving/grpc_rest_gateway/compiler/package.scala
@@ -9,28 +9,19 @@ import scala.jdk.CollectionConverters._
 
 package object compiler {
 
-  def extractPath(http: HttpRule): String =
-    http.getPatternCase match {
-      case PatternCase.GET    => http.getGet
-      case PatternCase.PUT    => http.getPut
-      case PatternCase.POST   => http.getPost
-      case PatternCase.DELETE => http.getDelete
-      case PatternCase.PATCH  => http.getPatch
-      case _                  => ""
-    }
-
   private def extractPathInternal(http: HttpRule) =
     http.getPatternCase match {
-      case PatternCase.GET    => (PatternCase.GET, http.getGet)
-      case PatternCase.PUT    => (PatternCase.PUT, http.getPut)
-      case PatternCase.POST   => (PatternCase.POST, http.getPost)
-      case PatternCase.DELETE => (PatternCase.DELETE, http.getDelete)
-      case PatternCase.PATCH  => (PatternCase.PATCH, http.getPatch)
-      case _                  => (PatternCase.PATTERN_NOT_SET, "")
+      case PatternCase.GET    => (PatternCase.GET, http.getGet, "")
+      case PatternCase.PUT    => (PatternCase.PUT, http.getPut, http.getBody)
+      case PatternCase.POST   => (PatternCase.POST, http.getPost, http.getBody)
+      case PatternCase.DELETE => (PatternCase.DELETE, http.getDelete, "")
+      case PatternCase.PATCH  => (PatternCase.PATCH, http.getPatch, "")
+      case _                  => (PatternCase.PATTERN_NOT_SET, "", "")
     }
 
-  def extractPaths(m: MethodDescriptor): Seq[(PatternCase, String)] = {
+  def extractPaths(m: MethodDescriptor): Seq[(PatternCase, String, String)] = {
     val http = m.getOptions.getExtension(AnnotationsProto.http)
     extractPathInternal(http) +: http.getAdditionalBindingsList.asScala.map(extractPathInternal).toSeq
   }
+
 }
