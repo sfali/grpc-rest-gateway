fb6b159d282cddfdab67b993c11ee88614b9bced
sfali <syed.f.ali@improving.com>
2024-11-10 07:44:15 -0500

    move to different package #11

--- code-gen/src/main/scala/grpc_rest_gateway/gatewayGen.scala
+++ code-gen/src/main/scala/grpc_rest_gateway/gatewayGen.scala
@@ -1,24 +1,26 @@
-package com.improving.grpc_rest_gateway
+package grpc_rest_gateway
 
 import com.improving.grpc_rest_gateway.compiler.BuildInfo
 import protocbridge.{Artifact, SandboxedJvmGenerator}
 import scalapb.GeneratorOption
 
-object swaggerGen {
+object gatewayGen {
   def apply(options: GeneratorOption*): (SandboxedJvmGenerator, Seq[String]) =
     (
       SandboxedJvmGenerator.forModule(
         "scala",
         Artifact(
           BuildInfo.organization,
-          "codegen_2.12",
+          "grpc-rest-gateway-code-gen_2.12",
           BuildInfo.version
         ),
-        "com.improving.grpc_rest_gateway.compiler.SwaggerGenerator$",
-        com.improving.grpc_rest_gateway.compiler.SwaggerGenerator.suggestedDependencies
+        "com.improving.grpc_rest_gateway.compiler.GatewayGenerator$",
+        com.improving.grpc_rest_gateway.compiler.GatewayGenerator.suggestedDependencies
       ),
       options.map(_.toString)
     )
 
-  def apply(options: Set[GeneratorOption] = Set.empty): (SandboxedJvmGenerator, Seq[String]) = apply(options.toSeq: _*)
+  def apply(
+    options: Set[GeneratorOption] = Set.empty
+  ): (SandboxedJvmGenerator, Seq[String]) = apply(options.toSeq: _*)
 }
--- code-gen/src/main/scala/grpc_rest_gateway/swaggerGen.scala
+++ code-gen/src/main/scala/grpc_rest_gateway/swaggerGen.scala
@@ -1,26 +1,24 @@
-package com.improving.grpc_rest_gateway
+package grpc_rest_gateway
 
 import com.improving.grpc_rest_gateway.compiler.BuildInfo
 import protocbridge.{Artifact, SandboxedJvmGenerator}
 import scalapb.GeneratorOption
 
-object gatewayGen {
+object swaggerGen {
   def apply(options: GeneratorOption*): (SandboxedJvmGenerator, Seq[String]) =
     (
       SandboxedJvmGenerator.forModule(
         "scala",
         Artifact(
           BuildInfo.organization,
-          "grpc-rest-gateway-code-gen_2.12",
+          "codegen_2.12",
           BuildInfo.version
         ),
-        "com.improving.grpc_rest_gateway.compiler.GatewayGenerator$",
-        com.improving.grpc_rest_gateway.compiler.GatewayGenerator.suggestedDependencies
+        "com.improving.grpc_rest_gateway.compiler.SwaggerGenerator$",
+        com.improving.grpc_rest_gateway.compiler.SwaggerGenerator.suggestedDependencies
       ),
       options.map(_.toString)
     )
 
-  def apply(
-    options: Set[GeneratorOption] = Set.empty
-  ): (SandboxedJvmGenerator, Seq[String]) = apply(options.toSeq: _*)
+  def apply(options: Set[GeneratorOption] = Set.empty): (SandboxedJvmGenerator, Seq[String]) = apply(options.toSeq: _*)
 }
