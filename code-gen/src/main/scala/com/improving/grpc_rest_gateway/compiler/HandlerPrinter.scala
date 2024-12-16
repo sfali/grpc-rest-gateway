4be73e3e74a33d677eb949e98dfee153e7d33a44
sfali <syed.f.ali@improving.com>
2024-12-16 10:29:28 -0500

    moving result function to HandlerPrinter

--- code-gen/src/main/scala/com/improving/grpc_rest_gateway/compiler/HandlerPrinter.scala
+++ code-gen/src/main/scala/com/improving/grpc_rest_gateway/compiler/HandlerPrinter.scala
@@ -3,8 +3,17 @@ package grpc_rest_gateway
 package compiler
 
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse
+import com.improving.grpc_rest_gateway.compiler.utils.Formatter
 
 trait HandlerPrinter {
 
-  val result: CodeGeneratorResponse.File
+  protected val content: String
+  protected val outputFileName: String
+
+  lazy val result: CodeGeneratorResponse.File = {
+    val b = CodeGeneratorResponse.File.newBuilder()
+    b.setName(outputFileName)
+    b.setContent(Formatter.format(content))
+    b.build()
+  }
 }
