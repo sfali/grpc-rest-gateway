143914ddbb4fb93dfcce332137da9d834b3c7cfe
sfali <syed.f.ali@improving.com>
2025-02-23 17:41:55 -0500

    check for hardTerminationDeadline config

--- runtime-core/src/main/scala/com/improving/grpc_rest_gateway/runtime/core/HttpSettings.scala
+++ runtime-core/src/main/scala/com/improving/grpc_rest_gateway/runtime/core/HttpSettings.scala
@@ -10,10 +10,15 @@ import scala.concurrent.duration.*
 case class HttpSettings(host: String, port: Int, hardTerminationDeadline: FiniteDuration)
 
 object HttpSettings {
-  def apply(config: Config): HttpSettings =
+  def apply(config: Config): HttpSettings = {
+    val hardTerminationDeadline =
+      if (config.hasPath("hard-termination-deadline"))
+        FiniteDuration(config.getDuration("hard-termination-deadline").toSeconds, SECONDS)
+      else 10.seconds
     HttpSettings(
       host = config.getString("host"),
       port = config.getInt("port"),
-      hardTerminationDeadline = FiniteDuration(config.getDuration("hard-termination-deadline").toSeconds, SECONDS)
+      hardTerminationDeadline = hardTerminationDeadline
     )
+  }
 }
