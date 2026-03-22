f33f26b1c046584ac4adfc3df2fe6237ec6a97bb
sfali <syed.f.ali@improving.com>
2026-03-22 14:45:46 -0400

    using pathEndOrSingleSlash instead of concat for better handling of single slash paths

--- code-gen/src/main/scala/com/improving/grpc_rest_gateway/compiler/akka_pekko/RouteGenerator.scala
+++ code-gen/src/main/scala/com/improving/grpc_rest_gateway/compiler/akka_pekko/RouteGenerator.scala
@@ -82,7 +82,7 @@ private class RouteGenerator(implicits: DescriptorImplicits, methods: List[Metho
       val methods = methodInfos.zipWithIndex
       val totalMethods = methods.size
       printer
-        .add("pathEnd {")
+        .add("pathEndOrSingleSlash {")
         .indent
         .when(totalMethods > 1)(_.add("concat(").indent)
         .print(methods) { case (p, (methodInfo, index)) => generateMethod(methodInfo, index, totalMethods)(p) }
