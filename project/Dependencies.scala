7117bdf298f4e1f3dd6ec06d185ac8e0fff471c5
sfali <syed.f.ali@improving.com>
2026-03-22 15:51:25 -0400

    update swagger ui version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val ScalaTest = "3.2.19"
     val Slf4j = "2.0.17"
     val Sttp = "3.11.0"
-    val SwaggerUi = "5.32.0"
+    val SwaggerUi = "5.32.1"
     val TypesafeConfig = "1.4.6"
   }
 
