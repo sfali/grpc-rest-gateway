4479b16ac19a9499fe3c2a139a47b376b718acf4
sfali <syed.f.ali@improving.com>
2026-03-23 12:27:29 -0400

    Move SwaggerHandler route to end of route list to allow gRPC routes to be tried first

--- runtime-pekko/src/main/scala-2.13/com/improving/grpc_rest_gateway/runtime/server/GatewayServer.scala
+++ runtime-pekko/src/main/scala-2.13/com/improving/grpc_rest_gateway/runtime/server/GatewayServer.scala
@@ -31,7 +31,7 @@ class GatewayServer(
     implicit val ec: ExecutionContext = sys.classicSystem.dispatcher
     val handlerRoutes = handlers.map(_.route)
     val routes =
-      if (openApiSettings.enabled) SwaggerHandler(openApiSettings.specsDirectory, handlers).route +: handlerRoutes
+      if (openApiSettings.enabled) handlerRoutes :+ SwaggerHandler(openApiSettings.specsDirectory, handlers).route
       else handlerRoutes
     val eventualBinding = Http()
       .newServerAt(host, port)
